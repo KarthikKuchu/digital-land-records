43ec8a0309e75d75b71fade3df7d55c8d5450c4e
kuchu <karthikkuchu1818@gmail.com>
2026-02-04 11:56:57 +0530

    Fix navbar links for HashRouter

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
-function Navbar() {
-    return(
-        <nav className="navbar">
-            <div className="logo">Land Records</div>
 
-            <ul className="nav-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/services">Services</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="contact">Contact</a></li>
-            </ul>
-        </nav>
-    );
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        Land Records
+      </div>
+
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
+
 export default Navbar;
